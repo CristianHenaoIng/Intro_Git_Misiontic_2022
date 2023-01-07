fb0088df54945b76d2f2d61d20b1f242dc159a9a
CristianHenaoIng <cdhenaop@unal.edu.co>
2023-01-07 07:37:22 -0500

    Actualizacion carpeta

---
+++ intro-git/HolaMundo.java
@@ -0,0 +1,10 @@
+class holaMundo {
+    public static void main(String[] args){
+        System.out.println();
+        saludar(); //se replica en la otra rama dev
+    }
+
+    public void saludar(){
+        console.log("");
+    }
+}
